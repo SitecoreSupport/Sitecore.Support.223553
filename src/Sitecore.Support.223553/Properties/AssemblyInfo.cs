accfcef9292f5abb04633ef91ad0dd0fead91a31
DK\EVS <evs@sitecore.com>
2018-06-05 14:00:24 +0300

    Initial commit with fix

---
+++ src/Sitecore.Support.223553/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.223553")]
+[assembly: AssemblyProduct("Sitecore.Support.223553")]
+[assembly: ComVisible(false)]
